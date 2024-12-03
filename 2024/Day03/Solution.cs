80a0beb1b3d3128d4cf6c7f199ff6db3fb657b2e
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-12-03 07:48:29 +0100

    solve 2024/03/02

--- 2024/Day03/Solution.cs
+++ 2024/Day03/Solution.cs
@@ -17,7 +17,32 @@ class Solution : Solver {
     }
 
     public object PartTwo(string input) {
-        return 0;
+        var instructions = IntermediateInputParser(input);
+        var pairs = new List<int[]>();
+        var isDisabled = false;
+
+        foreach (var instr in instructions) {
+            var instructionType = GetInstruction(instr);
+            switch (instructionType)
+            {
+                case "don't":
+                    isDisabled = true;
+                    continue;
+                case "do":
+                    isDisabled = false;
+                    continue;
+                case "mul": {
+                    if (!isDisabled) {
+                        var pair = GetPair(instr);
+                        pairs.Add(pair);
+                    }
+
+                    break;
+                }
+            }
+        }
+        
+        return pairs.Select(pair => pair[0] * pair[1]).Sum();
     }
 
     private static IEnumerable<int[]> InputParser(string input) {
@@ -31,5 +56,24 @@ class Solution : Solver {
             var matches = Regex.Matches(instruction.Value, instrDelimiter);
             return matches.Select(match => int.Parse(match.Value)).ToArray();
         });
+    }
+
+    private static IEnumerable<string> IntermediateInputParser(string input) {
+        string delimiter = @"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)";
+        var matchCollection = Regex.Matches(input, delimiter);
+	
+        return matchCollection.Select(match => match.Value);
+    }
+    
+    private static string GetInstruction(string input) {
+        string delimiter = @"(.+)\(.*\)";
+        var matches = Regex.Match(input, delimiter);
+        return matches.Groups[1].Value;
+    }
+
+    private static int[] GetPair(string input) {
+        string instrDelimiter = @"\d{1,3}";
+        var matches = Regex.Matches(input, instrDelimiter);
+        return matches.Select(match => int.Parse(match.Value)).ToArray();
     }
 }
